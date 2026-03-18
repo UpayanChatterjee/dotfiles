3a368eda7c095cda655a6664e5ad07ddf5d963c3
Tony Stark <upayanc9@kgpian.iitkgp.ac.in>
2026-03-18 19:21:59 +0530

    chore(python): set python3_host_prog path
    Configures the Python 3 host provider to use a specific conda
    environment path.

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ require("config.lazy")
 -- Ensure termguicolors is enabled if not already
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
+vim.g.python3_host_prog = vim.fn.expand("/home/tony/.conda/envs/nvim/bin/python3")
 
 require("nvim-highlight-colors").setup({})
